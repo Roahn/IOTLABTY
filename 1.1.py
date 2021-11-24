8e3cc1ceda2d9a71cadfbafd8003eec926e1d0df
Roahn <rohan.magar19@vit.edu>
2021-11-24 14:37:48 +0530

    Square root Calculator completed

---
+++ 1.1.py
@@ -0,0 +1,11 @@
+#Find out square root of given number
+import math;
+no = 0
+ret=0;
+print("---------------SQURE ROOT Calculator----------------")
+print("Enter Number :")
+no =int(input());
+
+ret = math.sqrt(no);
+
+print(ret);
