ba660667d084a648a54c9139b9c1c187cc4a6fd9
Roahn <rohan.magar19@vit.edu>
2021-11-24 15:38:51 +0530

    Completed 1.4.py finding area of triangle

---
+++ 1.4.py
@@ -0,0 +1,10 @@
+#find area of triangle
+height =0
+base = 0
+print("Enter height of triangle")
+height = int(input())
+print("Enter base of triangle")
+base = int(input())
+
+area = (height*base)/2
+print("area of triangle is",area)
