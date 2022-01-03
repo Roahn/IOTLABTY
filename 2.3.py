7c5bf0ab588b0460ad0edb3e951c4b6f77681821
Roahn <72019287+Roahn@users.noreply.github.com>
2022-01-03 17:45:31 +0530

    Changes in 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,11 +1,28 @@
 #take 2-3 sentences and print them in front of each other .Calculate all the words in each senrtence and
 #letters in individual words and capitalize each word in each sentence
 
-str1 = input()
-str2 = input()
+inp_string_1 = input() 
+inp_string_2 = input()
+inp_string_3 = input()
+inp_string_4 = input()
 
-print(str1 +" "+str2)
 
-for i in str:
-    if(i == " "):
-        print("GGG")
+string = inp_string_1 + inp_string_2 + inp_string_3 + inp_string_4
+print('The entered string after concatenation is - ', string)
+
+a = len(string.split())
+print('No. of words in the given string are - ', str(a))
+print('Total no. of letters are - ', len(string))
+
+print(string.title())
+print()
+
+print(string.upper())
+print()
+
+print(string.lower())
+print()
+
+String = string.split()
+print(String)
+[len(i) for i in String]
